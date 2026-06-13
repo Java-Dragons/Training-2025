41c5fcab93690b712b82fe65f5614f887c6764b7
Java-Dragons <javadragonsrobotics@gmail.com>
2026-06-13 15:35:33 -0400

    Created motors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTrain.java
@@ -1,4 +1,14 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
 public class DriveTrain {
+    private DcMotor FrontRM = null;
+    private DcMotor FrontLM = null;
+    private DcMotor BackRM = null;
+    private DcMotor BackLM = null;
+
+    private LinearOpMode opMode = null;
+
 }
